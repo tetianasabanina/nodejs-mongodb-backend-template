bd4d005373b729e29041b1333427095644afc329
tatianasabanina <tatiana08st@gmail.com>
2021-06-13 18:30:48 +0300

    install eslint and fix

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const config = require('config');
+
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
 
     console.log('MongoDB connected...');
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     // Exit process wíth failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
